60c99aea8feffbc005d391dff31f7ef1cf0bc3bb
Sara Keating <sjkeatingc@gmail.com>
2021-08-03 21:27:15 -0400

    Started working on backend more

--- pokeform.py
+++ pokeform.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/pokeinfo", methods=['GET'])
 def pokeinfo(name):
-    return
+    return render_template('pokeinfo.html')
 
 if __name__ == "__main__":
     app.run()
