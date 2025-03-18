241ebf39e89026b4b088ddcd45d0b1f4ad71fa50
pixolotyl <pixolotl@pm.me>
2025-03-18 15:07:27 -0400

    Fixed encoding error so csv can open in Windows

--- backend/pokeform.py
+++ backend/pokeform.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route("/api/<name>")
 def pokeinfo(name):
     ## Grabbing the info to be displayed from the csv
-    file = "pokedex.csv"
-    with open(file, "r") as pokedict:
+    file = "backend/pokedex.csv"
+    with open(file, encoding='cp437') as pokedict:
         ## Checking to see if name in search bar is a pokemon  in csv
         for line in csv.DictReader(pokedict):
             if name.lower() == line["name"].lower():
